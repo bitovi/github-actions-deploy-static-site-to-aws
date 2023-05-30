4b3a6eea0b2169c5b869387677fa5a910cb85dfe
LeoDiazL <116020815+LeoDiazL@users.noreply.github.com>
2023-05-30 20:12:25 -0300

    Fixing bucket destroy

---
+++ scripts/destroy_tf_state_bucket.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -e
+
+GITHUB_IDENTIFIER="$($GITHUB_ACTION_PATH/scripts/generate_identifier.sh)"
+GITHUB_IDENTIFIER="$($GITHUB_ACTION_PATH/scripts/generate_identifier.sh 30)"
+
+# Generate TF_STATE_BUCKET ID if empty 
+if [ -z "${TF_STATE_BUCKET}" ]; then
+  #  Add trailing id depending on name length - See AWS S3 bucket naming rules
+  if [[ ${#GITHUB_IDENTIFIER} < 55 ]]; then
+    export TF_STATE_BUCKET="${GITHUB_IDENTIFIER}-tf-state"
+  else
+    export TF_STATE_BUCKET="${GITHUB_IDENTIFIER}-tf"
+  fi
+fi
+
+
+aws s3 rb s3://$TF_STATE_BUCKET --force

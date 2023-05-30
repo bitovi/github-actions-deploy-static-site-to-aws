76e4626a5359e11164734cd27b776abcd08668d3
LeoDiazL <116020815+LeoDiazL@users.noreply.github.com>
2023-05-30 19:12:40 -0300

    Fixing variables.tf

--- terraform_code/variables.tf
+++ terraform_code/variables.tf
@@ -24,14 +24,8 @@ variable "aws_tf_state_bucket" {
   description = "Bucket to store tf-state file for the deployment"
 }
 
-variable "aws_spa_file_sources" {
+variable "aws_spa_source_folder" {
   description = "Sources to be stored in the bucket. Coma sepparated list of files possible."
-  type        = string
-  default     = ""
-}
-
-variable "aws_spa_file_keys" {
-  description = "Key names for stored objects in the bucket. Will be the names in the bucket."
   type        = string
   default     = ""
 }

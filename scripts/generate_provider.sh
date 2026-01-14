da0cae142c328df42fd1bb75f8341573e361aaee
LeoDiazL <leo@bitovi.com>
2026-01-14 11:46:55 -0300

    Fixing backend storage for tf

--- scripts/generate_provider.sh
+++ scripts/generate_provider.sh
@@ -98,11 +98,8 @@ terraform {
     }
   }
 
-  backend "s3" {
-    region  = "${AWS_DEFAULT_REGION}"
-    bucket  = "${TF_STATE_BUCKET}"
-    key     = "$(generate_tf_state_file_name site)"
-    encrypt = true #AES-256encryption
+  backend "local" {
+    path = "terraform.tfstate"
   }
 }
 
